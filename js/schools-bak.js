eb57eb2e22e202bcd711bc751f4124b9c8a0e832
章鱼DS <zhangkaihao2000@qq.com>
2019-08-11 21:53:53 +0800

    最后还是按名字排序吧

---
+++ js/schools-bak.js
@@ -0,0 +1,53 @@
+var schools = [
+    { 'person': '林银燕', 'school': '浙江工业大学', 'major': '' },
+    { 'person': '蒋霆龙', 'school': '浙江工业大学', 'major': '土木工程（中外合作办学）' },
+
+    { 'person': '杨　柳', 'school': '浙江工商大学', 'major': '计算机专业' },
+    { 'person': '王程可', 'school': '浙江工商大学', 'major': '电子信息类' },
+
+    { 'person': '张佳宁', 'school': '浙江农林大学', 'major': '计算机科学与技术（三位一体）' },
+    { 'person': '王　懿', 'school': '浙江农林大学', 'major': '' },
+
+    { 'person': '沈汝奕', 'school': '浙江理工大学', 'major': '' },
+    { 'person': '宋　琳', 'school': '浙江传媒学院', 'major': '数字媒体技术' },
+    { 'person': '罗浩天', 'school': '浙江海洋大学', 'major': '轮机工程' },
+    { 'person': '谢易作', 'school': '浙江警官职业学院', 'major': '' },
+    { 'person': '吕宏波', 'school': '浙江万里学院', 'major': '' },
+    { 'person': '冯超丹', 'school': '嘉兴学院', 'major': '' },
+
+    
+    { 'person': '邵琪雅', 'school': '杭州电子科技大学', 'major': '会计类' },
+    { 'person': '颜祺淞', 'school': '杭州电子科技大学', 'major': '电子信息类（三位一体）' },
+    { 'person': '陈黎莎', 'school': '杭州电子科技大学', 'major': ''},
+    
+    { 'person': '陈佳超', 'school': '温州大学', 'major': '土木工程' },
+    { 'person': '钟江涛', 'school': '温州医科大学', 'major': '临床医学' },
+
+    { 'person': '张晨昱', 'school': '长安大学', 'major': '' },
+    { 'person': '章楷豪', 'school': '西安电子科技大学', 'major': '电子信息类(物理与光电工程)' },
+    { 'person': '何雨晴', 'school': '西北工业大学明德学院', 'major': '' },
+    { 'person': '徐艳琪', 'school': '西南交通大学', 'major': '' },
+    { 'person': '郑成锴', 'school': '四川农业大学成都校区', 'major': '' },
+
+    { 'person': '包相江', 'school': '江南大学', 'major': '光电与信息工程' },
+
+    { 'person': '吕思佳', 'school': '大连海事大学', 'major': '' },
+    { 'person': '顾子涵', 'school': '上海海事大学', 'major': '' },
+
+    { 'person': '朱天渝', 'school': '南京邮电大学', 'major': '材料化学' },
+    { 'person': '郭　颂', 'school': '太原理工大学', 'major': '' },
+
+    { 'person': '郑虞颖', 'school': '宁波大学', 'major': '数学与应用数学' },
+    { 'person': '严梦柯', 'school': '宁波工程学院风华校区', 'major': '' },
+
+    { 'person': '宋珂源', 'school': '哈尔滨工业大学', 'major': '工科试验班(计算机与电子通信)' },
+    { 'person': '凌瑞添', 'school': '东北大学秦皇岛分校', 'major': '机械类' },
+    { 'person': '张靖赟', 'school': '北京航空航天大学', 'major': '工科试验班类(信息类)' },
+    { 'person': '陈　磊', 'school': '北京中瑞酒店管理学院', 'major': '' },
+    { 'person': '陈虞滇', 'school': '南京师范大学', 'major': '' },
+
+    { 'person': '叶相湾', 'school': '兰州理工大学', 'major': '金属材料工程' },
+    { 'person': '董晨骐', 'school': '中南林业科技大学', 'major': '' },
+    { 'person': '应波波', 'school': '钓鱼岛', 'major': '学长加油！' },
+    { 'person': '罗　迈', 'school': '伦敦大学学院', 'major': '艺术学院' }
+];
